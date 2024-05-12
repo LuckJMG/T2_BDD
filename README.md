d555963bf8429f0f4ef0cab1f23f0e774e46cc44
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2024-05-12 16:38:47 -0400

    commit inicial

---
+++ README.md
@@ -0,0 +1,6 @@
+## Integrantes
+
+- Lucas Mosquera, 202273504-k
+- Joaquín Dominguez,
+
+## Instrucciones
---
+++ php/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+		<h1>Test</h1>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+echo $conn->query("SHOW DATABASES;")
+?>
+    </body>
+</html>
